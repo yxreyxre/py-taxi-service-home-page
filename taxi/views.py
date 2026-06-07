ccfb77db44a07b6c3fdbbc0454a731e15f4a944a
yxreyxre <sashamahasla@gmail.com>
2026-06-07 22:04:57 +0300

    Solution#3

--- taxi/views.py
+++ taxi/views.py
@@ -2,10 +2,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from taxi.models import Driver, Car, Manufacturer
 
-# Create your views here.
 
-
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
     num_cars = Car.objects.count()
